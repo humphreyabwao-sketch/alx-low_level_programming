70693c387db34443a243ccde3137be3ac85aff74
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-16 16:19:18 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  * Return: Always 0.
  */                                                                                                                                                                                                                                                                                                                                                                                                        
 int main(void)                                                                                                                                                                                                                                                                                                                                                                                             
-{                                                                                                                                                                                                               puts("\"Programming is like building a multilingual puzzle");
-	    return (0);                                                                                                                                                                               
+{                                                                                                                                                                                                             puts("\"Programming is like building a multilingual");
+	 return (0);                                                                                                                                                                               
 }
