988aaca4c5ea726e346f0c85b96f82feab361f48
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-16 16:10:19 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
+                                                                                                                                                                                                      
 /**
- *  * main prints "Programming is like building a multilingual
- *  puzzle, followed by a new line.
- *  Return Alway 0.
- */
-    
-  
-int main(void)                                                                                                                                                                                      
-
-
-{
-
-            puts("\"Programming is like building a multilingual puzzle");
+ * main - print "Programming is like building a multilingual
+ * puzzle, followed by a new line.
+ * Return: Always 0.
+ */                                                                                                                                                                                                                                                                                                                                                                                                        
+int main(void)                                                                                                                                                                                                                                                                                                                                                                                             
+{                                                                                                                                                                                                               puts("\"Programming is like building a multilingual puzzle");
 	    return (0);                                                                                                                                                                               
 }
