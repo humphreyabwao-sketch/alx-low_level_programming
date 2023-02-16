d07a462db3a2b55bb6c4a75773dca6ac52fc8014
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-16 15:26:52 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *  * main - "Programming is like building a multilingual puzzle."
  *   *
  *    * Return: Always 0 (Success)
- *     */
-int main(void)
+ *     */                                                                                                                                                                                                                                                                                                                                                                                                     
+int main(void)                                                                                                                                                                                                                                                                                                                                                                                                 
 { 
 	   puts("\"Programming is like building a multilingual puzzle");
 	    
-	    return(0);
+	    return (0);
 }
