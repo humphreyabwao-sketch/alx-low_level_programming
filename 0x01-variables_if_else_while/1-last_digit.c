943d69628a2db02949681967cda9342410063c5f
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-17 18:47:02 +0300

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printif("Last digit of %d is %d and is less than 6 and not 0/n",
+		printf("Last digit of %d is %d and is less than 6 and not 0/n",
 				n, n % 10);
 	}
 	else
