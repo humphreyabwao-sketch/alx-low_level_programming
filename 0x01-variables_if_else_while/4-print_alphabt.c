622349106d432b23b09c0445dea7deae85826b3f
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-18 01:10:02 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'q' && alphabets != 'e')
+		{
+			putchar(alphabets);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
