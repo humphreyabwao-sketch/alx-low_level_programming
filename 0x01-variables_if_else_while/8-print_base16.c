412972ef495b5b55997a72588adb813dbc518d16
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-18 02:16:18 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+		putchar(numbers + '0');
+	putchar('\n');
+	return (0);
+}
