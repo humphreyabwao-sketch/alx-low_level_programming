63abc69a2a914805c208d3de8ddd0cc768d85175
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-18 02:19:43 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int numbers;
@@ -12,5 +13,6 @@ int main(void)
 	for (numbers = 0; numbers < 10; numbers++)
 		putchar(numbers + '0');
 	putchar('\n');
+
 	return (0);
 }
