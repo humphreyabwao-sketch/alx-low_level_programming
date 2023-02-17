95b521825fad428e4e5cb6adebabec086df99ca3
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-02-18 02:41:27 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char numbers;
+
+	for (numbers = '0'; numbers <= '9'; numbers++)
+		putchar(numbers);
+	for (numbers = 'a'; numbers <= 'f'; numbers++)
+		putchar(numbers);
+	putchar('\n');
+	return (0);
+}
