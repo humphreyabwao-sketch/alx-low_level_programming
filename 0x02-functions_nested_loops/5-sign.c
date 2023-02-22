ca91b9b3d8fab533b9e21f40e70bce19b379d41a
Your Name <humphreyabwao@gmail.com>
2023-02-22 09:04:22 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
-	return (1);
+		_putchar('+');
+		return (1);
 	}
-	else if
-	(n < 0);
+	else if (n == 0)
 	{
-	_putchar(45);
-	return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-	_putchar
-	return (0);
+		_putchar('-');
+		return (0);
 	}
 }
