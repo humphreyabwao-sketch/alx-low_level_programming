957967504f457245f6ef1ae0c2696dc27e9b4da3
Your Name <humphreyabwao@gmail.com>
2023-02-22 13:52:19 -0800

    0-putchar.c

--- _putchar.c
+++ _putchar.c
@@ -5,9 +5,9 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriatley.
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
