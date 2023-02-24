293bc7929342959f822bea104af135ac6491ecc4
Your Name <humphreyabwao@gmail.com>
2023-02-23 20:14:01 -0800

    1-isgidit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _isdigit - Entry point
+  * @c: - character to be checked
+  * Return: 1 if c is a digit, otherwise 0
+  */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
