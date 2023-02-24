afc8742dcb7080a757bbfd47e1df9b25a84c6f5f
Your Name <humphreyabwao@gmail.com>
2023-02-23 20:50:59 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_square - a function that prints a square
+  * @size: single input digit.
+  * Return: Always 0 (success)
+  */
+
+void print_square(int size)
+{
+	int m, n;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m = 0; m <= size; m++)
+		{
+			for (n = 0; n < size; n++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
