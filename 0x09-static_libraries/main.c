4baf07b8ea63ea63e90a66def05bea948d15750d
humphreyabwao-sketch <your.email>
2023-03-10 09:57:24 -0800

    create_static_lib.sh

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the\n"
+"best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
