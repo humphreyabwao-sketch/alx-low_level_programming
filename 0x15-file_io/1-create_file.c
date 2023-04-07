7b2c48774fe714200eff6f8e98476e5e886739a9
humpreyabwao-sketch <humphreyabwao@gmail.com>
2023-04-07 12:48:18 -0700

    ./b hello world

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,6 @@ if (filename == NULL)
 printf("Error: filename is NULL\n");
 return (-1);
 }
-
 if (text_content != NULL)
 {
 while (text_content[len] != '\0')
@@ -29,14 +28,12 @@ while (text_content[len] != '\0')
 len++;
 }
 }
-
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 {
 printf("Error: could not create file\n");
 return (-1);
 }
-
 w = write(fd, text_content, len);
 if (w == -1)
 {
@@ -44,7 +41,6 @@ printf("Error: could not write to file\n");
 close(fd);
 return (-1);
 }
-
 close(fd);
 return (1);
 }
