5a154aeceafcc366708713cda53a91321e2df803
humpreyabwao-sketch <humphreyabwao@gmail.com>
2023-04-07 12:53:43 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * create_file - Creates a new file and writes text content to it.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file.
- *
- * Return: On success, returns 1. On failure, returns -1.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, w, len = 0;
+	int fd, w, len = 0;
 
-if (filename == NULL)
-{
-printf("Error: filename is NULL\n");
-return (-1);
-}
-if (text_content != NULL)
-{
-while (text_content[len] != '\0')
-{
-len++;
-}
-}
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1)
-{
-printf("Error: could not create file\n");
-return (-1);
-}
-w = write(fd, text_content, len);
-if (w == -1)
-{
-printf("Error: could not write to file\n");
-close(fd);
-return (-1);
-}
-close(fd);
-return (1);
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
