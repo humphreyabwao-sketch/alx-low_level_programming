f6bcdd3ca91613bdf3d5dd7960c08bdaff119c23
humpreyabwao-sketch <humphreyabwao@gmail.com>
2023-05-22 13:02:07 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+/* operations.c */
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of @a and @b.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between @a and @b.
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of @a and @b.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of dividing @a by @b.
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - Computes the remainder of dividing two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The remainder of dividing @a by @b.
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
